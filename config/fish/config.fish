934ff38cb42f1812fbc5056dc0693abee07c0a72
adrianpriza-ai <adrianpriza18@gmail.com>
2026-04-07 18:38:40 +0700

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
-oh-my-posh init fish --config /home/momoi/.config/fish/poshthemes/paradox1.omp.json | source
+oh-my-posh init fish --config $HOME/.config/fish/poshthemes/paradox1.omp.json | source
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
